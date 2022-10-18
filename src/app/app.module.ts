835c0331d9692ade00399b49ad730b2154abe531
LOISEAU Thomas <thomas.loisea@gmail.com>
2022-10-18 18:40:40 +0200

    Install Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,26 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 
 import { LayoutsModule } from "./layouts/layouts.module";
+import { initializeApp, provideFirebaseApp } from "@angular/fire/app";
+import { environment } from "../environments/environment";
+import { provideAnalytics, getAnalytics, ScreenTrackingService, UserTrackingService } from "@angular/fire/analytics";
+import { provideAuth, getAuth } from "@angular/fire/auth";
+import { provideFirestore, getFirestore } from "@angular/fire/firestore";
+import { provideFunctions, getFunctions } from "@angular/fire/functions";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, LayoutsModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    LayoutsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions()),
+  ],
+  providers: [ScreenTrackingService, UserTrackingService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
